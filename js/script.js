301cd06e96f752abdeaac7785e61ede4c775e012
Adrian Kotowski <adekkotowski1@wp.pl>
2025-02-20 22:20:26 +0100

    Add counting won rounds

--- js/script.js
+++ js/script.js
@@ -24,32 +24,54 @@ function buttonClicked(argButtonName) {
 }
     
 function displayResult(argPlayerMove, argComputerMove) {
-  console.log('wywołano funkcję displayResult z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
-    printMessage('Wygrywasz!');
-  } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
-    printMessage('Wygrywasz!'); 
-  } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
-    printMessage('Wygrywasz!'); 
-  } else if (argPlayerMove == argComputerMove) {
-    printMessage('Jest remis!!!');  
-  } else {
-    printMessage('Przegrywasz :(');
-  }
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+        console.log('wywołano funkcję displayResult z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+        if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+            printMessage('Wygrywasz!');
+            playerScore++; // Zwiększenie liczby wygranych gracza
+        } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+            printMessage('Wygrywasz!');
+            playerScore++; // Zwiększenie liczby wygranych gracza  
+        } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+            printMessage('Wygrywasz!');
+            playerScore++; // Zwiększenie liczby wygranych gracza  
+        } else if (argPlayerMove == argComputerMove) {
+            printMessage('Jest remis!!!');
+        } else {
+            printMessage('Przegrywasz :(');
+            computerScore++;
+        }
+            printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+    
+            updateScore();
+    }
+    
+    playerMove = argButtonName;
+    console.log('ruch gracza to: ' + playerMove);
+    randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log('wylosowana liczba to: ' + randomNumber);
+    computerMove = getMoveName(randomNumber);
+    console.log('ruch komputera to: ' + computerMove);
+    displayResult(playerMove, computerMove);
+
 }
 
-playerMove = argButtonName;
-console.log('ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
-console.log('ruch komputera to: ' + computerMove);
-displayResult(playerMove, computerMove);   
+   
     
-}
+
 
 buttonTest.addEventListener('click', function () { buttonClicked('Test button'); });
 buttonPaper.addEventListener('click', function () { buttonClicked('papier'); });
 buttonRock.addEventListener('click', function () { buttonClicked('kamień'); });
 buttonScissors.addEventListener('click', function () { buttonClicked('nożyce'); });
+
+// Zmienne przechowujące liczbę wygranych rund
+var playerScore = 0;
+var computerScore = 0;
+
+// Pobranie elementu do wyświetlania wyniku
+var resultDiv = document.getElementById('result');
+
+// Funkcja aktualizująca wynik gry na stronie
+function updateScore() {
+  resultDiv.innerHTML = `Wynik: ${playerScore} - ${computerScore}`;
+}
